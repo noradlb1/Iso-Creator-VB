40dc964fea8ec2c644df3a429dbdf8c2bb9e6325
MONSTERMC <74623428+noradlb1@users.noreply.github.com>
2024-07-05 12:50:03 +0200

    Iso-Creator-VB
    Iso-Creator-VB

---
+++ Iso Creator MONSTERMC/Form1.Designer.vb
@@ -0,0 +1,172 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.textBoxVolumeName = New System.Windows.Forms.TextBox()
+        Me.txtPath = New System.Windows.Forms.TextBox()
+        Me.labelStatus = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.progressBar = New System.Windows.Forms.ProgressBar()
+        Me.lblStatus = New System.Windows.Forms.Label()
+        Me.LinkDiscord = New System.Windows.Forms.LinkLabel()
+        Me.LinkTelegram = New System.Windows.Forms.LinkLabel()
+        Me.LinkYoutube = New System.Windows.Forms.LinkLabel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'textBoxVolumeName
+        '
+        Me.textBoxVolumeName.Location = New System.Drawing.Point(11, 72)
+        Me.textBoxVolumeName.Name = "textBoxVolumeName"
+        Me.textBoxVolumeName.Size = New System.Drawing.Size(188, 20)
+        Me.textBoxVolumeName.TabIndex = 20
+        Me.textBoxVolumeName.Text = "Volume Name"
+        '
+        'txtPath
+        '
+        Me.txtPath.Location = New System.Drawing.Point(12, 43)
+        Me.txtPath.Name = "txtPath"
+        Me.txtPath.Size = New System.Drawing.Size(188, 20)
+        Me.txtPath.TabIndex = 19
+        Me.txtPath.Text = "txtPath"
+        '
+        'labelStatus
+        '
+        Me.labelStatus.AutoSize = True
+        Me.labelStatus.Location = New System.Drawing.Point(156, 23)
+        Me.labelStatus.Name = "labelStatus"
+        Me.labelStatus.Size = New System.Drawing.Size(43, 13)
+        Me.labelStatus.TabIndex = 18
+        Me.labelStatus.Text = "Status: "
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(206, 69)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(89, 23)
+        Me.Button2.TabIndex = 17
+        Me.Button2.Text = "Button2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(206, 40)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(89, 23)
+        Me.Button1.TabIndex = 16
+        Me.Button1.Text = "Select Folder"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'progressBar
+        '
+        Me.progressBar.Location = New System.Drawing.Point(12, 98)
+        Me.progressBar.Name = "progressBar"
+        Me.progressBar.Size = New System.Drawing.Size(283, 23)
+        Me.progressBar.TabIndex = 22
+        '
+        'lblStatus
+        '
+        Me.lblStatus.AutoSize = True
+        Me.lblStatus.Location = New System.Drawing.Point(9, 23)
+        Me.lblStatus.Name = "lblStatus"
+        Me.lblStatus.Size = New System.Drawing.Size(43, 13)
+        Me.lblStatus.TabIndex = 23
+        Me.lblStatus.Text = "Status: "
+        '
+        'LinkDiscord
+        '
+        Me.LinkDiscord.AutoSize = True
+        Me.LinkDiscord.Location = New System.Drawing.Point(8, 137)
+        Me.LinkDiscord.Name = "LinkDiscord"
+        Me.LinkDiscord.Size = New System.Drawing.Size(43, 13)
+        Me.LinkDiscord.TabIndex = 24
+        Me.LinkDiscord.TabStop = True
+        Me.LinkDiscord.Text = "Discord"
+        '
+        'LinkTelegram
+        '
+        Me.LinkTelegram.AutoSize = True
+        Me.LinkTelegram.Location = New System.Drawing.Point(121, 137)
+        Me.LinkTelegram.Name = "LinkTelegram"
+        Me.LinkTelegram.Size = New System.Drawing.Size(51, 13)
+        Me.LinkTelegram.TabIndex = 25
+        Me.LinkTelegram.TabStop = True
+        Me.LinkTelegram.Text = "Telegram"
+        '
+        'LinkYoutube
+        '
+        Me.LinkYoutube.AutoSize = True
+        Me.LinkYoutube.Location = New System.Drawing.Point(251, 137)
+        Me.LinkYoutube.Name = "LinkYoutube"
+        Me.LinkYoutube.Size = New System.Drawing.Size(47, 13)
+        Me.LinkYoutube.TabIndex = 26
+        Me.LinkYoutube.TabStop = True
+        Me.LinkYoutube.Text = "Youtube"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(6, 161)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(180, 13)
+        Me.Label1.TabIndex = 27
+        Me.Label1.Text = "Contact me to program your own tool"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(305, 180)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.LinkYoutube)
+        Me.Controls.Add(Me.LinkTelegram)
+        Me.Controls.Add(Me.LinkDiscord)
+        Me.Controls.Add(Me.lblStatus)
+        Me.Controls.Add(Me.textBoxVolumeName)
+        Me.Controls.Add(Me.txtPath)
+        Me.Controls.Add(Me.labelStatus)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.progressBar)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Iso Creator MONSTERMC"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents textBoxVolumeName As TextBox
+    Friend WithEvents txtPath As TextBox
+    Friend WithEvents labelStatus As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents progressBar As ProgressBar
+    Friend WithEvents lblStatus As Label
+    Friend WithEvents LinkDiscord As LinkLabel
+    Friend WithEvents LinkTelegram As LinkLabel
+    Friend WithEvents LinkYoutube As LinkLabel
+    Friend WithEvents Label1 As Label
+End Class
---
+++ Iso Creator MONSTERMC/Form1.vb
@@ -0,0 +1,147 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Drawing
+Imports System.Windows.Forms
+Imports System.IO
+Imports System.Threading
+Imports BER.CDCat.Export
+Imports System.ComponentModel
+Partial Public Class Form1
+    Inherits Form
+    Private Sub statusgood(log As String)
+        'Create For Log Window
+        lblStatus.Text = log & " -Status OK"
+        'Do Events
+        Call Application.DoEvents()
+    End Sub
+    Private Sub statusbad(log As String)
+        lblStatus.Text = log & " -Status BAD"
+        'Do Events
+        Call Application.DoEvents()
+    End Sub
+    Private m_thread As Thread = Nothing
+    Private m_creator As IsoCreator.IsoCreator = Nothing
+    Private Delegate Sub SetLabelDelegate(text As String)
+    Private Delegate Sub SetNumericValueDelegate(value As Integer)
+    Private Sub creator_Abort(sender As Object, e As AbortEventArgs)
+        MessageBox.Show(e.Message, "Abort", MessageBoxButtons.OK, MessageBoxIcon.Stop)
+        MessageBox.Show("The ISO creating process has been stopped.", "Abort", MessageBoxButtons.OK, MessageBoxIcon.Stop)
+        Button2.Invoke(New Action(Sub() Button2.Enabled = False))
+        progressBar.Invoke(New Action(Sub() progressBar.Value = 0))
+        progressBar.Invoke(New Action(Sub() progressBar.Maximum = 0))
+        labelStatus.Invoke(New Action(Sub() labelStatus.Text = "Process not started"))
+    End Sub
+
+    Private Sub creator_Finished(sender As Object, e As FinishEventArgs)
+        MessageBox.Show(e.Message, "Finish", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        Button2.Invoke(New Action(Sub() Button2.Enabled = False))
+        progressBar.Invoke(New Action(Sub() progressBar.Value = 0))
+        labelStatus.Invoke(New Action(Sub() labelStatus.Text = "Process not started"))
+        lblStatus.Invoke(New Action(Sub() lblStatus.Text = ""))
+    End Sub
+    Private Sub SetLabelStatus(text As String)
+        labelStatus.Text = text
+        labelStatus.Refresh()
+    End Sub
+    Private Sub SetProgressValue(value As Integer)
+        progressBar.Value = value
+    End Sub
+    Private Sub SetProgressMaximum(maximum As Integer)
+        progressBar.Maximum = maximum
+    End Sub
+    Private Sub creator_Progress(sender As Object, e As ProgressEventArgs)
+        If Not Equals(e.Action, Nothing) Then
+            If Not InvokeRequired Then
+                SetLabelStatus(e.Action)
+            Else
+                Invoke(New SetLabelDelegate(AddressOf SetLabelStatus), e.Action)
+            End If
+        End If
+        If e.Maximum <> -1 Then
+            If Not InvokeRequired Then
+                SetProgressMaximum(e.Maximum)
+            Else
+                Invoke(New SetNumericValueDelegate(AddressOf SetProgressMaximum), e.Maximum)
+            End If
+        End If
+        If Not InvokeRequired Then
+            progressBar.Value = If(e.Current <= progressBar.Maximum, e.Current, progressBar.Maximum)
+        Else
+            Dim value = If(e.Current <= progressBar.Maximum, e.Current, progressBar.Maximum)
+            Invoke(New SetNumericValueDelegate(AddressOf SetProgressValue), value)
+        End If
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim dialog As FolderBrowserDialog = New FolderBrowserDialog()
+        If dialog.ShowDialog() = DialogResult.OK Then
+            txtPath.Text = dialog.SelectedPath
+            textBoxVolumeName.Text = New DirectoryInfo(dialog.SelectedPath).Name & ".iso"
+            Dim result = MessageBox.Show("Do you want to create an ISO of this folder?", "gg", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+            If result = DialogResult.Yes Then
+                Dim savedialog = MessageBox.Show("Do you want to save the .iso in a custom location?", "ggggg", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+                If savedialog = DialogResult.No Then
+                    If m_thread Is Nothing OrElse Not m_thread.IsAlive Then
+                        If Not Equals(textBoxVolumeName.Text.Trim(), "") Then
+                            m_thread = New Thread(New ParameterizedThreadStart(AddressOf m_creator.Folder2Iso))
+                            m_thread.Start(New IsoCreator.IsoCreator.IsoCreatorFolderArgs(txtPath.Text, Application.StartupPath & "\" & textBoxVolumeName.Text, textBoxVolumeName.Text))
+
+                            Button2.Enabled = True
+                        Else
+                            MessageBox.Show("Please insert a name for the volume", "No volume name", MessageBoxButtons.OK, MessageBoxIcon.Hand)
+                        End If
+                    End If
+                ElseIf result = DialogResult.Yes Then
+                    Dim savepath = String.Empty
+                    Dim savename = String.Empty
+                    Dim saveme As SaveFileDialog = New SaveFileDialog()
+                    saveme.Filter = "PS3 Disk Images (*.iso)|*.iso"
+                    If saveme.ShowDialog() = DialogResult.OK Then
+                        savepath = saveme.FileName
+                        savename = New FileInfo(saveme.FileName).Name
+                        textBoxVolumeName.Text = savename
+                        'TabControl1.SelectedIndex = 3
+                    End If
+                    If m_thread Is Nothing OrElse Not m_thread.IsAlive Then
+                        If Not Equals(textBoxVolumeName.Text.Trim(), "") Then
+                            m_thread = New Thread(New ParameterizedThreadStart(AddressOf m_creator.Folder2Iso))
+                            m_thread.Start(New IsoCreator.IsoCreator.IsoCreatorFolderArgs(txtPath.Text, savepath, savename))
+
+                            Button2.Enabled = True
+                        End If
+                    End If
+                End If
+            End If
+        End If
+    End Sub
+    Private Sub Form1_Closing(sender As Object, e As CancelEventArgs) Handles Me.Closing
+        If m_creator IsNot Nothing AndAlso m_thread IsNot Nothing AndAlso m_thread.IsAlive Then
+            m_thread.Abort()
+        End If
+    End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        m_creator = New IsoCreator.IsoCreator()
+        AddHandler m_creator.Progress, New ProgressDelegate(AddressOf creator_Progress)
+        AddHandler m_creator.Finish, New FinishDelegate(AddressOf creator_Finished)
+        AddHandler m_creator.Abort, New AbortDelegate(AddressOf creator_Abort)
+    End Sub
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If m_creator IsNot Nothing AndAlso m_thread IsNot Nothing AndAlso m_thread.IsAlive Then
+            m_thread.Abort()
+            Button2.Enabled = False
+        End If
+    End Sub
+
+    Private Sub LinkDiscord_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkDiscord.LinkClicked
+
+    End Sub
+
+    Private Sub LinkTelegram_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkTelegram.LinkClicked
+
+    End Sub
+
+    Private Sub LinkYoutube_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkYoutube.LinkClicked
+
+    End Sub
+
+End Class
+
